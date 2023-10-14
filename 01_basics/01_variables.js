1d24c22b0a712fef365394a9d01177198c5d45a1
Sreenalini Nambiar <84523800+Sreenalini120@users.noreply.github.com>
2023-10-14 19:08:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail = "sreenalinipr@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+
+//accountId = 2// not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="289289"
+accountCity="banguluru"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
